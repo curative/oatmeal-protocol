66596dacf8596d1eea3a36c0412b98d0d0d441df
Isaac Turner <turner.isaac@gmail.com>
2019-05-03 14:09:15 -0700

    Set pypi username

--- python/setup.py
+++ python/setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="oatmeal-USERNAME",
+    name="oatmeal-Isaac.Turner",
     version="1.0",
     author="Isaac Turner",
     author_email="turner.isaac@gmail.com",
-    description="A simple mechanism to autoconnect and control Arduino devices from Python.",
+    description="A protocol to control and communicate with Arduino devices from Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shielddx/oatmeal-protocol",
