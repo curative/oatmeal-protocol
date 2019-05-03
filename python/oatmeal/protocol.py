ae9f1a5ca28e9c0769ddac722f49222b41b24f30
Isaac Turner <turner.isaac@gmail.com>
2019-05-03 10:51:38 -0700

    Fix MAX_HEARTBEAT_GAP_SEC member mis-named in abstract OatmealBgMsgHandlerBase

--- python/oatmeal/protocol.py
+++ python/oatmeal/protocol.py
@@ -1061,7 +1061,7 @@ class OatmealBgMsgHandlerBase(ABC):
 
     @property
     @abstractmethod
-    def max_gap_sec(self) -> float:
+    def MAX_HEARTBEAT_GAP_SEC(self) -> float:
         """
         Defines the maximum time that is permitted to pass without a heartbeat
         (or log message) before a heartbeat is considered to have been missed
